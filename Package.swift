24819c7556810ca4df715b2deaa1523611c4feed
Stephen F. Booth <me@sbooth.org>
2025-11-16 11:11:12 -0600

    Add Package manifest

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+	name: "CXXAudioToolbox",
+	products: [
+		// Products define the executables and libraries a package produces, making them visible to other packages.
+		.library(
+			name: "CXXAudioToolbox",
+			targets: [
+				"CXXAudioToolbox",
+			]
+		),
+	],
+	dependencies: [
+		.package(url: "https://github.com/sbooth/CXXCoreAudio", branch: "main")
+	],
+	targets: [
+		// Targets are the basic building blocks of a package, defining a module or a test suite.
+		// Targets can depend on other targets in this package and products from dependencies.
+		.target(
+			name: "CXXAudioToolbox",
+			dependencies: [
+				"CXXCoreAudio",
+			],
+			linkerSettings: [
+				.linkedFramework("AudioToolbox"),
+			]
+		),
+		.testTarget(
+			name: "CXXAudioToolboxTests",
+			dependencies: [
+				"CXXAudioToolbox",
+			],
+			swiftSettings: [
+				.interoperabilityMode(.Cxx),
+			]
+		),
+	],
+	cxxLanguageStandard: .cxx17
+)
