e6197e6c2f947196d7f8fa761f038e69e8958a02
Stephen Booth <me@sbooth.org>
2026-01-02 17:59:11 -0600

    Add SPDX tags (#20)

--- Sources/CXXAudioToolbox/CAAudioFormat.cpp
+++ Sources/CXXAudioToolbox/CAAudioFormat.cpp
@@ -1,7 +1,8 @@
 //
-// Copyright © 2021-2025 Stephen F. Booth
+// SPDX-FileCopyrightText: 2021 Stephen F. Booth <contact@sbooth.dev>
+// SPDX-License-Identifier: MIT
+//
 // Part of https://github.com/sbooth/CXXAudioToolbox
-// MIT license
 //
 
 #import "CAAudioFormat.hpp"
