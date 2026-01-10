afc5161bb874250e0ce942ff477a52468e9d9036
Stephen Booth <me@sbooth.org>
2026-01-10 12:34:40 -0600

    Update `CXXCoreAudio` to version 0.5.1 (#22)

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
 		),
 	],
 	dependencies: [
-		.package(url: "https://github.com/sbooth/CXXCoreAudio", from: "0.3.0")
+		.package(url: "https://github.com/sbooth/CXXCoreAudio", from: "0.5.1")
 	],
 	targets: [
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
