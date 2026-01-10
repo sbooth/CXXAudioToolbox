bef028179fa40173e823d0e9c78eb98389dd302e
Stephen Booth <me@sbooth.org>
2026-01-10 12:51:30 -0600

    Move method bodies out of class declarations (#23)

--- Sources/CXXAudioToolbox/include/CXXAudioToolbox/ExtAudioFileWrapper.hpp
+++ Sources/CXXAudioToolbox/include/CXXAudioToolbox/ExtAudioFileWrapper.hpp
@@ -26,70 +26,92 @@ public:
 	ExtAudioFileWrapper& operator=(const ExtAudioFileWrapper&) = delete;
 
 	/// Move constructor.
-	ExtAudioFileWrapper(ExtAudioFileWrapper&& other) noexcept
-	: extAudioFile_{other.release()}
-	{}
+	ExtAudioFileWrapper(ExtAudioFileWrapper&& other) noexcept;
 
 	/// Move assignment operator.
-	ExtAudioFileWrapper& operator=(ExtAudioFileWrapper&& other) noexcept
-	{
-		reset(other.release());
-		return *this;
-	}
+	ExtAudioFileWrapper& operator=(ExtAudioFileWrapper&& other) noexcept;
 
 	/// Calls ExtAudioFileDispose on the managed ExtAudioFile object.
-	~ExtAudioFileWrapper() noexcept
-	{
-		reset();
-	}
+	~ExtAudioFileWrapper() noexcept;
 
 	/// Creates an extended audio file wrapper managing an existing ExtAudioFile object.
-	explicit ExtAudioFileWrapper(ExtAudioFileRef _Nullable extAudioFile) noexcept
-	: extAudioFile_{extAudioFile}
-	{}
+	explicit ExtAudioFileWrapper(ExtAudioFileRef _Nullable extAudioFile) noexcept;
 
 	/// Returns true if the managed ExtAudioFile object is not null.
-	explicit operator bool() const noexcept
-	{
-		return extAudioFile_ != nullptr;
-	}
+	explicit operator bool() const noexcept;
 
 	/// Returns the managed ExtAudioFile object.
-	operator ExtAudioFileRef _Nullable() const noexcept
-	{
-		return extAudioFile_;
-	}
+	operator ExtAudioFileRef _Nullable() const noexcept;
 
 	/// Returns the managed ExtAudioFile object.
-	ExtAudioFileRef _Nullable get() const noexcept
-	{
-		return extAudioFile_;
-	}
+	ExtAudioFileRef _Nullable get() const noexcept;
 
 	/// Replaces the managed ExtAudioFile object with another ExtAudioFile object.
 	/// @note The object assumes responsibility for disposing of the passed ExtAudioFile object using ExtAudioFileDispose.
-	void reset(ExtAudioFileRef _Nullable extAudioFile = nullptr) noexcept
-	{
-		if(auto oldExtAudioFile = std::exchange(extAudioFile_, extAudioFile); oldExtAudioFile)
-			ExtAudioFileDispose(oldExtAudioFile);
-	}
+	void reset(ExtAudioFileRef _Nullable extAudioFile = nullptr) noexcept;
 
 	/// Swaps the managed ExtAudioFile object with the managed ExtAudioFile object from another extended audio file wrapper.
-	void swap(ExtAudioFileWrapper& other) noexcept
-	{
-		std::swap(extAudioFile_, other.extAudioFile_);
-	}
+	void swap(ExtAudioFileWrapper& other) noexcept;
 
 	/// Releases ownership of the managed ExtAudioFile object and returns it.
 	/// @note The caller assumes responsibility for disposing of the returned ExtAudioFile object using ExtAudioFileDispose.
-	ExtAudioFileRef _Nullable release() noexcept
-	{
-		return std::exchange(extAudioFile_, nullptr);
-	}
+	ExtAudioFileRef _Nullable release() noexcept;
 
 private:
 	/// The managed ExtAudioFile object.
 	ExtAudioFileRef _Nullable extAudioFile_{nullptr};
 };
+
+// MARK: - Implementation -
+
+inline ExtAudioFileWrapper::ExtAudioFileWrapper(ExtAudioFileWrapper&& other) noexcept
+: extAudioFile_{other.release()}
+{}
+
+inline ExtAudioFileWrapper& ExtAudioFileWrapper::operator=(ExtAudioFileWrapper&& other) noexcept
+{
+	reset(other.release());
+	return *this;
+}
+
+inline ExtAudioFileWrapper::~ExtAudioFileWrapper() noexcept
+{
+	reset();
+}
+
+inline ExtAudioFileWrapper::ExtAudioFileWrapper(ExtAudioFileRef _Nullable extAudioFile) noexcept
+: extAudioFile_{extAudioFile}
+{}
+
+inline ExtAudioFileWrapper::operator bool() const noexcept
+{
+	return extAudioFile_ != nullptr;
+}
+
+inline ExtAudioFileWrapper::operator ExtAudioFileRef _Nullable() const noexcept
+{
+	return extAudioFile_;
+}
+
+inline ExtAudioFileRef _Nullable ExtAudioFileWrapper::get() const noexcept
+{
+	return extAudioFile_;
+}
+
+inline void ExtAudioFileWrapper::reset(ExtAudioFileRef _Nullable extAudioFile) noexcept
+{
+	if(auto oldExtAudioFile = std::exchange(extAudioFile_, extAudioFile); oldExtAudioFile)
+		ExtAudioFileDispose(oldExtAudioFile);
+}
+
+inline void ExtAudioFileWrapper::swap(ExtAudioFileWrapper& other) noexcept
+{
+	std::swap(extAudioFile_, other.extAudioFile_);
+}
+
+inline ExtAudioFileRef _Nullable ExtAudioFileWrapper::release() noexcept
+{
+	return std::exchange(extAudioFile_, nullptr);
+}
 
 } /* namespace CXXAudioToolbox */
