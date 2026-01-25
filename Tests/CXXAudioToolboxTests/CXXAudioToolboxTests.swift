451eb31b327d711b85a4174e8325fa4591d31912
Stephen Booth <me@sbooth.org>
2026-01-25 12:32:21 -0600

    Tabs to spaces

--- Tests/CXXAudioToolboxTests/CXXAudioToolboxTests.swift
+++ Tests/CXXAudioToolboxTests/CXXAudioToolboxTests.swift
@@ -9,23 +9,23 @@ import Testing
 @testable import CXXAudioToolbox
 
 @Suite struct CXXCoreAudioTests {
-	@Test func audioConverter() async {
-		let converter = CXXAudioToolbox.CAAudioConverter()
-		#expect(converter.__convertToBool() == false)
-	}
+    @Test func audioConverter() async {
+        let converter = CXXAudioToolbox.CAAudioConverter()
+        #expect(converter.__convertToBool() == false)
+    }
 
-	@Test func audioFile() async {
-		let af = CXXAudioToolbox.CAAudioFile()
-		#expect(af.__convertToBool() == false)
-	}
+    @Test func audioFile() async {
+        let af = CXXAudioToolbox.CAAudioFile()
+        #expect(af.__convertToBool() == false)
+    }
 
-	@Test func extAudioFile() async {
-		let eaf = CXXAudioToolbox.CAExtAudioFile()
-		#expect(eaf.__convertToBool() == false)
-	}
+    @Test func extAudioFile() async {
+        let eaf = CXXAudioToolbox.CAExtAudioFile()
+        #expect(eaf.__convertToBool() == false)
+    }
 
-	@Test func auGraph() async {
-		let graph = CXXAudioToolbox.CAAUGraph()
-		#expect(graph.__convertToBool() == false)
-	}
+    @Test func auGraph() async {
+        let graph = CXXAudioToolbox.CAAUGraph()
+        #expect(graph.__convertToBool() == false)
+    }
 }
