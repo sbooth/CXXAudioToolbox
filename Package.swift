83c27e15b269e7df68c588dcab6c997e716629e6
Stephen Booth <me@sbooth.org>
2026-01-25 11:24:28 -0600

    Tabs to spaces

--- Package.swift
+++ Package.swift
@@ -9,43 +9,43 @@
 import PackageDescription
 
 let package = Package(
-	name: "CXXAudioToolbox",
-	products: [
-		// Products define the executables and libraries a package produces, making them visible to other packages.
-		.library(
-			name: "CXXAudioToolbox",
-			targets: [
-				"CXXAudioToolbox",
-			]
-		),
-	],
-	dependencies: [
-		.package(url: "https://github.com/sbooth/CXXCoreAudio", from: "0.5.1")
-	],
-	targets: [
-		// Targets are the basic building blocks of a package, defining a module or a test suite.
-		// Targets can depend on other targets in this package and products from dependencies.
-		.target(
-			name: "CXXAudioToolbox",
-			dependencies: [
-				"CXXCoreAudio",
-			],
-			cxxSettings: [
-				.headerSearchPath("include/CXXAudioToolbox"),
-			],
-			linkerSettings: [
-				.linkedFramework("AudioToolbox"),
-			]
-		),
-		.testTarget(
-			name: "CXXAudioToolboxTests",
-			dependencies: [
-				"CXXAudioToolbox",
-			],
-			swiftSettings: [
-				.interoperabilityMode(.Cxx),
-			]
-		),
-	],
-	cxxLanguageStandard: .cxx17
+    name: "CXXAudioToolbox",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "CXXAudioToolbox",
+            targets: [
+                "CXXAudioToolbox",
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/sbooth/CXXCoreAudio", from: "0.5.1")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "CXXAudioToolbox",
+            dependencies: [
+                "CXXCoreAudio",
+            ],
+            cxxSettings: [
+                .headerSearchPath("include/CXXAudioToolbox"),
+            ],
+            linkerSettings: [
+                .linkedFramework("AudioToolbox"),
+            ]
+        ),
+        .testTarget(
+            name: "CXXAudioToolboxTests",
+            dependencies: [
+                "CXXAudioToolbox",
+            ],
+            swiftSettings: [
+                .interoperabilityMode(.Cxx),
+            ]
+        ),
+    ],
+    cxxLanguageStandard: .cxx17
 )
